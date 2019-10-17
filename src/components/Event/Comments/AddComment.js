e0581d2749e9bc28c0a093ed29a5d6fbb0edd225
Quan Nguyen <quhung1989@gmail.com>
2019-10-16 17:32:43 -0700

    add comments working again

--- src/components/Event/Comments/AddComment.js
+++ src/components/Event/Comments/AddComment.js
@@ -1,70 +1,77 @@
-import React, { Component } from 'react';
-import Button from '../../UI/Button';
-import classes from './AddComment.module.css';
-import Input from '../../UI/Input';
-import { inputChanger } from '../../../utils/inputChanger';
+import React, { Component } from "react";
+import Button from "../../UI/Button";
+import classes from "./AddComment.module.css";
+import Input from "../../UI/Input";
+import { inputChanger } from "../../../utils/inputChanger";
 
 class AddComment extends Component {
-	state = {
-		comment: {
-			eleType: 'input',
-			eleConfig: {
-				type: 'text',
-				placeholder: 'Comment',
-			},
-			value: '',
-			validation: {
-				required: true,
-				type: 'general',
-			},
-			valid: false,
-		}
-	}
+   state = {
+      comment: {
+         text: {
+            eleType: "input",
+            eleConfig: {
+               type: "text",
+               placeholder: "Comment",
+            },
+            value: "",
+            validation: {
+               required: true,
+               type: "general",
+            },
+            valid: false,
+         },
+      },
+   };
 
-	render() {
-		let comment = this.state.comment;
-		let inputs;
+   render() {
+      let comment;
+      let commentState = this.state.comment.text;
 
-		if (this.props.username) {
-			inputs = (
-				<form>
-					<div className={classes.AddCommentTitle}>Add a Comment!</div>
-					<Input
-						eleType={comment.eleType}
-						value={comment.value}
-						invalid={!comment.valid}
-						changed={(event) => {
-							this.setState({ comment: inputChanger(event, 'comment', this.state, comment.validation.type) });
-						}}
-						eleConfig={comment.eleConfig}
-					/>
-					<Button clicked={(() => this.addComment())} type={'submit'}>
-						Submit
-          </Button>
-				</form>
-			)
-		} else {
-			inputs = (
-				<div className={classes.AddCommentTitle}>Please log in to enter a comment!</div>
-			)
-		}
+      if (this.props.username) {
+         comment = (
+            <form>
+               <div className={classes.AddCommentTitle}>Add a Comment!</div>
+               <Input
+                  eleType={commentState.eleType}
+                  eleConfig={commentState.eleConfig}
+                  invalid={!commentState.valid}
+                  value={commentState.value}
+                  changed={(event) => {
+                     this.setState({
+                        comment: inputChanger(
+                           event,
+                           "text",
+                           this.state.comment,
+                           commentState.validation.type
+                        ),
+                     });
+                  }}
+               />
+               <Button clicked={() => this.addComment()} type={"submit"}>
+                  Submit
+               </Button>
+            </form>
+         );
+      } else {
+         comment = (
+            <div className={classes.AddCommentTitle}>
+               Please log in to enter a comment!
+            </div>
+         );
+      }
 
-		return (
-			<div className={classes.AddComment}>
-				{inputs}
-			</div>
-		)
-	}
+      return <div className={classes.AddComment}>{comment}</div>;
+   }
 
-	addComment = () => {
-		if (this.props.username) {
-			const comment = this.state.comment;
-			let newState = JSON.parse(JSON.stringify(this.state.comment));
-			newState.value = '';
-			this.setState({ comment: newState });
-			this.props.addComment(comment);
-		}
-	}
+   addComment = () => {
+      if (this.props.username) {
+         const comment = this.state.comment;
+         let newState = JSON.parse(JSON.stringify(this.state.comment));
+         newState.value = "";
+         this.setState({ comment: newState });
+         this.props.addComment(comment);
+      }
+   };
 }
 
 export default AddComment;
--- src/components/Event/Comments/Comments.js
+++ src/components/Event/Comments/Comments.js
@@ -1,138 +1,145 @@
-import React, {Component} from 'react';
-import classes from './Comments.module.css';
-import Comment from './Comment';
-import axios from 'axios';
-import AddComment from '../Comments/AddComment';
+import React, { Component } from "react";
+import classes from "./Comments.module.css";
+import Comment from "./Comment";
+import axios from "axios";
+import AddComment from "../Comments/AddComment";
 
 class Comments extends Component {
-  state = {
-    eventId: null,
-    comments: [],
-    username: '',
-    commenters: [
-      {
-        username: 'name',
-        image: 'nam.png'
-      }
-    ],
-  }
+   state = {
+      eventId: null,
+      comments: [],
+      username: "",
+      commenters: [
+         {
+            username: "name",
+            image: "nam.png",
+         },
+      ],
+   };
 
-  render() {
-    let comments = [];
-    const commentsArray = this.state.comments;
+   render() {
+      let comments = [];
+      const commentsArray = this.state.comments;
 
-    let addComments = (
-      <AddComment
-        username={this.props.username}
-        addComment={this.addCommentHandler}/>
-    );
-    
-    for(let i = commentsArray.length - 1; i >= 0; i--){
-      const username = commentsArray[i].author.username;
-      const comment = this.state.commenters.find(commenter => this.findCommenter(commenter, username))
-      let image
-      if (comment) {
-        image = comment.image
-      }
-      
-      comments.push(
-        <Comment
-          username={username}
-          loggedInUser={this.props.username}
-          text={commentsArray[i].text}
-          deleteComment={() => this.deleteComment(commentsArray[i]._id)}
-          image={image}
-          // image={commentsArray[i].profileImg}
-          key={commentsArray[i]._id}/>
+      let addComments = (
+         <AddComment
+            username={this.props.username}
+            addComment={this.addCommentHandler}
+         />
       );
-    }
 
-    return (
-      <div className={classes.Comments}>
-        {addComments}
-        {comments}
-      </div>
-    );
-  }
+      for (let i = commentsArray.length - 1; i >= 0; i--) {
+         const username = commentsArray[i].author.username;
+         const comment = this.state.commenters.find((commenter) =>
+            this.findCommenter(commenter, username)
+         );
+         let image;
+         if (comment) {
+            image = comment.image;
+         }
 
-  UNSAFE_componentWillMount() {
-    this.setState({eventId: this.props.eventId},
-      () =>this.loadComments());
-  }
-
-  deleteComment = (id) => {
-    axios.delete(`/api/comments/delete-comment/${id}`)
-    .then(this.loadComments())
-    .catch(err => console.log(err));
-  }
-
-  loadComments = () => {
-    axios.get(`/api/comments/get-comments/${this.state.eventId}`)
-    .then((res) => {
-      this.setState({comments: res.data.comments});
-    })
-    .then(() => {
-      this.addCommenters()
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
-
-  addCommentHandler = (comment) => {
-    // eslint-disable-next-line
-    event.preventDefault()
-    let username = this.props.username;
-    let text = comment.value;
-    if (username && text) {
-      const COMMENT = {
-        username: username,
-        comment: text,
-      }
-      axios.post(`/api/comments/post-comment/${this.props.eventId}`, COMMENT)
-      .then(() => this.loadComments())
-      .catch(err => console.log(err))
-    }
-  }
-
-  getCommenterImage = (username) => {
-    let commenter = {}
-    axios.get(`/api/users/get-user-image/${username}`)
-    .then((res) => {
-      if (res.data) {
-        commenter.username = username
-        commenter.image = res.data
-        const commenters = [...this.state.comments]
-        commenters.push(commenter)
-        this.setState({commenters: commenters})
-      }
-    })
-    .catch(err => console.log(err));
-  }
-
-  addCommenters = () => {
-    let commentsArray = this.state.comments
-    const commenters = this.state.commenters
-
-    for(let i = commentsArray.length - 1; i >= 0; i--) {
-      let indexed
-      let username = commentsArray[i].author.username
-
-      for(let i = 0; i < commenters.length; i++) {
-        if (commenters[i].username === username) {
-          indexed = true
-        }
+         comments.push(
+            <Comment
+               username={username}
+               loggedInUser={this.props.username}
+               text={commentsArray[i].text}
+               deleteComment={() => this.deleteComment(commentsArray[i]._id)}
+               image={image}
+               // image={commentsArray[i].profileImg}
+               key={commentsArray[i]._id}
+            />
+         );
       }
 
-      if (!indexed) {
-        this.getCommenterImage(username)
-      }
-    }
-  }
+      return (
+         <div className={classes.Comments}>
+            {addComments}
+            {comments}
+         </div>
+      );
+   }
 
-  findCommenter = (commenter, username) => {
-    return commenter.username === username
-  }
+   UNSAFE_componentWillMount() {
+      this.setState({ eventId: this.props.eventId }, () => this.loadComments());
+   }
+
+   deleteComment = (id) => {
+      axios
+         .delete(`/api/comments/delete-comment/${id}`)
+         .then(this.loadComments())
+         .catch((err) => console.log(err));
+   };
+
+   loadComments = () => {
+      axios
+         .get(`/api/comments/get-comments/${this.state.eventId}`)
+         .then((res) => {
+            this.setState({ comments: res.data.comments });
+         })
+         .then(() => {
+            this.addCommenters();
+         })
+         .catch((err) => {
+            console.log(err);
+         });
+   };
+
+   addCommentHandler = (comment) => {
+      // eslint-disable-next-line
+      event.preventDefault();
+      let username = this.props.username;
+      let text = comment.text.value;
+      if (username && text) {
+         const COMMENT = {
+            username: username,
+            comment: text,
+         };
+         axios
+            .post(`/api/comments/post-comment/${this.props.eventId}`, COMMENT)
+            .then(() => this.loadComments())
+            .catch((err) => console.log(err));
+      }
+   };
+
+   getCommenterImage = (username) => {
+      let commenter = {};
+      axios
+         .get(`/api/users/get-user-image/${username}`)
+         .then((res) => {
+            if (res.data) {
+               commenter.username = username;
+               commenter.image = res.data;
+               const commenters = [...this.state.comments];
+               commenters.push(commenter);
+               this.setState({ commenters: commenters });
+            }
+         })
+         .catch((err) => console.log(err));
+   };
+
+   addCommenters = () => {
+      let commentsArray = this.state.comments;
+      const commenters = this.state.commenters;
+
+      for (let i = commentsArray.length - 1; i >= 0; i--) {
+         let indexed;
+         let username = commentsArray[i].author.username;
+
+         for (let i = 0; i < commenters.length; i++) {
+            if (commenters[i].username === username) {
+               indexed = true;
+            }
+         }
+
+         if (!indexed) {
+            this.getCommenterImage(username);
+         }
+      }
+   };
+
+   findCommenter = (commenter, username) => {
+      return commenter.username === username;
+   };
 }
 
-export default (Comments);
+export default Comments;
